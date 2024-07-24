91a12ef6a420744a1210f401f3955f07665921c2
Anurag22060 <anuragsawal03@gmail.com>
2024-07-24 10:18:02 +0530

    changes2

--- src/data/constants.js
+++ src/data/constants.js
@@ -216,7 +216,7 @@ export const projects = [
     description:
       "Discover a new era of video-sharing with our React-based YouTube clone. This project redefines the user experience with its intuitive interface, responsive design, and innovative features. From dynamic recommendations and real-time interactivity to advanced search capabilities, our clone is more than just a mimicry of popular platforms. Content creators benefit from easy video management, while viewers enjoy a seamless and personalized browsing experience. Powered by React on the frontend, a robust server on the backend, and secure authentication, this clone is designed to elevate your video-sharing journey. Join us and explore a world where sharing and discovering content is effortless and enjoyable.",
     image:
-      "https://drive.google.com/file/d/1-yf7xIZ8NA8OqCCtOYONnO_BPtD6n1tg/view?usp=sharing",
+      "https://raw.githubusercontent.com/Anurag22060/youtube_clone/master/src/Images/Youtube_clone.png",
     tags: [
        "React Js",
       "JavaScript",
